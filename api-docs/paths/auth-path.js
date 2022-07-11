ba9feb52de47d2de731c95e0825e6b1d4bbd563c
arieesetiawan <arieesetiawann@gmail.com>
2022-07-11 13:38:59 +0700

    Penambahan Order dan Items

--- api-docs/paths/auth-path.js
+++ api-docs/paths/auth-path.js
@@ -44,8 +44,9 @@ module.exports = {
             },
             404: {
               description: "Wrong Username or Password"
-            }
-          },            
+            },
+          }, 
+                     
       }     
     },
     '/auth/admin/register':{
@@ -160,6 +161,110 @@ module.exports = {
               },
               401: {
                 description: "invalid"
+              }
+            },            
+        }     
+      },
+      '/auth/account/login':{
+        post:{
+          tags: ["Auth"],
+          summary: "Login Customer",
+          description: "An endpoint to Login as Customer",
+          operationId: "Login Customer",
+          consumes: [
+            "application/json"
+          ],
+          produces: [
+            "application/json"
+          ],   
+          parameters: [
+              {
+                in: "path",
+                name: "email",
+                description: "Username",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              },
+              {
+                in: "path",
+                name: "password",
+                description: "Password",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              },
+          ],
+          responses: {
+              201: {
+                description: "Successfully Login",
+                content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/Auth2'
+                      }
+                    }
+                  },
+              },
+              401: {
+                description: "invalid"
+              },
+              404: {
+                description: "Wrong Username or Password"
+              }
+            },            
+        }     
+      },
+      '/auth/seller/login':{
+        post:{
+          tags: ["Auth"],
+          summary: "Login Seller",
+          description: "An endpoint to Login as Seller",
+          operationId: "Login Seller",
+          consumes: [
+            "application/json"
+          ],
+          produces: [
+            "application/json"
+          ],   
+          parameters: [
+              {
+                in: "path",
+                name: "email",
+                description: "Username",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              },
+              {
+                in: "path",
+                name: "password",
+                description: "Password",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              },
+          ],
+          responses: {
+              201: {
+                description: "Successfully Login",
+                content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/Auth2'
+                      }
+                    }
+                  },
+              },
+              401: {
+                description: "invalid"
+              },
+              404: {
+                description: "Wrong Username or Password"
               }
             },            
         }     
---
+++ api-docs/paths/item-path.js
@@ -0,0 +1,293 @@
+module.exports = {
+    '/items':{
+      post:{
+        tags: ["Item"],
+        summary: "Create Item",
+        description: "An endpoint to Create Item (Seller/Admin Access)",
+        operationId: "CreateItem",
+        consumes: [
+          "application/json"
+        ],
+        produces: [
+          "application/json"
+        ],   
+        parameters: [
+            {
+              in: "path",
+              name: "category_id",
+              description: "Item Category",
+              required: true,
+              schema: {
+                type: "string"
+              },
+              enum: [
+                "kategori 1",
+                "kategori 2",
+                "kategori 3"
+              ]
+            },
+            {
+              in: "path",
+              name: "code",
+              description: "Item Code",
+              required: true,
+              schema: {
+                type: "integer"
+              }
+            },
+            {
+              in: "path",
+              name: "seotitle",
+              description: "SEO Title",
+              required: true,
+              schema: {
+                type: "string"
+              }
+            },
+            {
+              in: "path",
+              name: "title",
+              description: "Title of Item",
+              required: true,
+              schema: {
+                type: "string"
+              }
+            },
+            {
+              in: "path",
+              name: "price",
+              description: "Price of Item",
+              required: true,
+              schema: {
+                type: "integer"
+              }
+            },
+            {
+              in: "path",
+              name: "qty",
+              description: "Quantity of Item",
+              required: true,
+              schema: {
+                type: "integer"
+              }
+            },
+        ],
+        responses: {
+            201: {
+              description: "Successfully Created Item",
+              content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/Item'
+                    }
+                  }
+                },
+            },
+            400: {
+              description: "Invalid"
+            },
+          },                      
+      },
+      get:{
+        tags: ["Item"],
+        summary: "Get Item",
+        description: "An endpoint to Get All Items (Seller/Admin/Cust Access)",
+        operationId: "GetItem",
+        consumes: [
+          "application/json"
+        ],
+        produces: [
+          "application/json"
+        ],   
+        responses: {
+            201: {
+              description: "Successfully Get All Items",
+              content: {
+                  'application/json': {
+                    schema: {
+                      $ref: '#/components/schemas/Item2'
+                    }
+                  }
+                },
+            },
+            400: {
+              description: "Invalid"
+            },
+          }, 
+        }     
+    },
+    '/items/{itemID}':{
+        get:{
+          tags: ["Item"],
+          summary: "Get Item by ID",
+          description: "An endpoint to get Item by ID (Seller/Admin/Cust Access)",
+          operationId: "GetItembyID",
+          consumes: [
+            "application/json"
+          ],
+          produces: [
+            "application/json"
+          ],   
+          parameters: [
+              {
+                in: "path",
+                name: "ItemID",
+                description: "ItemID",
+                required: true,
+                schema: {
+                  type: "integer"
+                }
+              },
+          ],
+          responses: {
+              201: {
+                description: "Successfully Found Item",
+                content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/Item2'
+                      }
+                    }
+                  },
+              },
+              401: {
+                description: "Invalid"
+              },
+              404: {
+                description: "Item not Found"
+              }
+            },            
+        },
+        put:{
+          tags: ["Item"],
+          summary: "Edit item by ID",
+          description: "An endpoint to edit Item by ID (Seller/Admin/Cust Access)",
+          operationId: "EditItembyID",
+          consumes: [
+            "application/json"
+          ],
+          produces: [
+            "application/json"
+          ],   
+          parameters: [
+              {
+                in: "path",
+                name: "ItemID",
+                description: "ItemID",
+                required: true,
+                schema: {
+                  type: "integer"
+                }
+              },
+          ],
+          responses: {
+              201: {
+                description: "Successfully Edited Item",
+                content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/Item2'
+                      }
+                    }
+                  },
+              },
+              401: {
+                description: "Invalid"
+              },
+              404: {
+                description: "Item not Found"
+              }
+            },            
+        },
+        delete:{
+          tags: ["Item"],
+          summary: "Delete Item by ID",
+          description: "An endpoint to delete Item by ID (Seller/Admin/Cust Access)",
+          operationId: "DeleteItembyID",
+          consumes: [
+            "application/json"
+          ],
+          produces: [
+            "application/json"
+          ],   
+          parameters: [
+              {
+                in: "path",
+                name: "ItemID",
+                description: "ItemID",
+                required: true,
+                schema: {
+                  type: "integer"
+                }
+              },
+          ],
+          responses: {
+              201: {
+                description: "Successfully Delete Item",
+                content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/Item3'
+                      }
+                    }
+                  },
+              },
+              401: {
+                description: "Invalid"
+              },
+              404: {
+                description: "Item not Found"
+              }
+            },            
+        },
+      },
+      '/items/addImage':{
+        post:{
+          tags: ["Item"],
+          summary: "Add Image",
+          description: "An endpoint to add Image to Item",
+          operationId: "addImageItem",
+          consumes: [
+            "application/json"
+          ],
+          produces: [
+            "application/json"
+          ],   
+          parameters: [
+              {
+                in: "path",
+                name: "item_ID",
+                description: "Item ID",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              },
+              {
+                in: "path",
+                name: "Picture",
+                description: "Link of Picture",
+                required: true,
+                schema: {
+                  type: "string"
+                }
+              },
+          ],
+          responses: {
+              201: {
+                description: "Successfully Register",
+                content: {
+                    'application/json': {
+                      schema: {
+                        $ref: '#/components/schemas/Item4'
+                      }
+                    }
+                  },
+              },
+              401: {
+                description: "invalid"
+              }
+            },            
+        }     
+      },
+}
