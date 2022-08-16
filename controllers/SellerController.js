a78a51541dada8e008d981a1188da4f3e41dbd21
Raeyhans <myweb.zone14@gmail.com>
2022-08-16 16:08:28 +0700

    update controller

--- controllers/SellerController.js
+++ controllers/SellerController.js
@@ -15,25 +15,25 @@ exports.registerSeller = async (req, res, next) => {
     // }
 
     try {
-        
-        const user = await db.Sellers.findOne({
-            where: {
-                email: body.email
-        }});
-            
-        if (user != null) {
-            return res.status(400).json({
-                error: 'Please choose another email.'
-            });
-        }
-        
         if (body.firstname == null || body.password == null || body.email == null || body.code == null) {
             return res.status(400).json({
                 status: 400,
                 msg: 'All field cannot empty.'
             });
         }
-            
+
+        const user = await db.Sellers.findOne({
+            where: {
+                email: body.email
+        }});
+
+        if (user != null) {
+            return res.status(400).json({
+                error: 'Please choose another email.'
+            });
+        }
+
+
         const hashToken = jwt.sign(body.email, body.password);
 
         await db.Sellers.create({
