82db3422566bcea8398faa35e9997517d69214bc
Raeyhans <myweb.zone14@gmail.com>
2022-08-22 08:17:24 +0700

    update email

--- controllers/CustomerController.js
+++ controllers/CustomerController.js
@@ -49,6 +49,7 @@ exports.registerCustomer = async (req, res, next) => {
 
         await sendEmail({
             to: body.email,
+            from: process.env.EMAIL_FROM,
             subject: 'Sign-up Verification',
             html: `<h4>Verify Email</h4>
                     <p>Thanks for registering!</p><p>Please use the below token to verify your email address with the <code>/account/verify/TOKEN</code> api route:</p>
